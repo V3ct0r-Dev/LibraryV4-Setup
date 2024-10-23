dd5b807a474c5b5a5194b4dfe4363e305cb16964
V3ct0r-Dev <00The_zV3ct0r.Xpl0iter_exe@proton.me>
2024-10-23 00:33:52 -0300

    Add files via upload

---
+++ Themes.lua
@@ -0,0 +1,20 @@
+local Themes = {
+  ["Default"] = {
+    ["Color Hub 1"] = Color3.fromRGB(0, 0, 0),
+    ["Color Hub 2"] = Color3.fromRGB(160, 32, 240),
+    ["Color Theme"] = Color3.fromRGB(0, 0, 0),
+    ["Color Text"] = Color3.fromRGB(255, 254, 255),
+    ["Color Dark Text"] = Color3.fromRGB(255, 255, 255),
+    ["Color Stroke"] = Color3.fromRGB(160, 32, 240),
+    ["Corner Radius"] = UDim.new(0, 5),
+    ["Font"] = {
+      Enum.Font.GothamBlack,
+      Enum.Font.GothamBold,
+      Enum.Font.GothamMedium
+    },
+    ["Stroke Thickness"] = 1,
+    ["Tween Style"] = Enum.EasingStyle.Quad
+  },
+}
+
+return Themes
