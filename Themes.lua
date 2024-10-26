9d157e617acadf1f2f227870e8fdea27b2f91353
V3ct0r-Dev <00The_zV3ct0r.Xpl0iter_exe@proton.me>
2024-10-26 12:43:46 -0300

    Update Themes.lua

--- Themes.lua
+++ Themes.lua
@@ -2,10 +2,10 @@ local Themes = {
   ["Default"] = {
     ["Color Hub 1"] = Color3.fromRGB(0, 0, 0),
     ["Color Hub 2"] = Color3.fromRGB(0, 0, 0),
-    ["Color Theme"] = Color3.fromRGB(255, 0, 0),
-    ["Color Text"] = Color3.fromRGB(255, 0, 0),
-    ["Color Dark Text"] = Color3.fromRGB(255, 0, 0),
-    ["Color Stroke"] = Color3.fromRGB(255, 0, 0),
+    ["Color Theme"] = Color3.fromRGB(0, 0, 255),
+    ["Color Text"] = Color3.fromRGB(0, 0, 255),
+    ["Color Dark Text"] = Color3.fromRGB(0, 0, 255),
+    ["Color Stroke"] = Color3.fromRGB(0, 0, 255),
     ["Corner Radius"] = UDim.new(0, 5),
     ["Font"] = {
       Enum.Font.GothamBlack,
