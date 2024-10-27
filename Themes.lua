50035f4aafab434a3d93f6e52ac2c694124356c4
V3ct0r-Dev <00The_zV3ct0r.Xpl0iter_exe@proton.me>
2024-10-27 11:21:03 -0300

    Update Themes.lua

--- Themes.lua
+++ Themes.lua
@@ -3,9 +3,9 @@ local Themes = {
     ["Color Hub 1"] = Color3.fromRGB(0, 0, 0),
     ["Color Hub 2"] = Color3.fromRGB(0, 0, 0),
     ["Color Theme"] = Color3.fromRGB(0, 0, 255),
-    ["Color Text"] = Color3.fromRGB(0, 0, 255),
-    ["Color Dark Text"] = Color3.fromRGB(0, 0, 255),
-    ["Color Stroke"] = Color3.fromRGB(0, 0, 255),
+    ["Color Text"] = Color3.fromRGB(255, 0, 0,
+    ["Color Dark Text"] = Color3.fromRGB(0, 240, 0),
+    ["Color Stroke"] = Color3.fromRGB(255, 0, 0),
     ["Corner Radius"] = UDim.new(0, 5),
     ["Font"] = {
       Enum.Font.FredokaOne,
